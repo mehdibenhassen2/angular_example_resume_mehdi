f48343a61e530da755ba2a888471d08ca470204d
Mehdi BEN HASSEN <m.ben.hassen80@gmail.com>
2024-03-26 00:22:57 -0400

    TTA-18 integrate contentfull in professional experience-task

--- src/app/Modules/professional-experience/tasks/tasks.component.ts
+++ src/app/Modules/professional-experience/tasks/tasks.component.ts
@@ -17,8 +17,8 @@ import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 interface CompanyNode {
   name: string;
+  logo?: string;
   children?: CompanyNode[];
-  logo: string;
 }
 
 /** Flat node with expandable and level information */
@@ -27,13 +27,7 @@ interface ExampleFlatNode {
   name: string;
   level: number;
 }
-
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
+let TREE_DATA: CompanyNode[] = [];
 @Component({
   selector: "app-tasks",
   templateUrl: "./tasks.component.html",
@@ -41,8 +35,7 @@ export interface Tile {
 })
 export class TasksComponent implements OnInit {
   private tableCollected: Observable<any>;
-
-  TREE_DATA: CompanyNode[] = [];
+  usedLanguage = this.translationEnFrService.language; // this one should be an observable
   private _transformer = (node: CompanyNode, level: number) => {
     return {
       expandable: !!node.children && node.children.length > 0,
@@ -51,6 +44,7 @@ export class TasksComponent implements OnInit {
       level: level,
     };
   };
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
 
   treeControl = new FlatTreeControl<ExampleFlatNode>(
     (node) => node.level,
@@ -66,55 +60,58 @@ export class TasksComponent implements OnInit {
 
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
-  tiles: Tile[] = [
-    { text: "One", cols: 3, rows: 1, color: "lightblue" },
-    { text: "Two", cols: 1, rows: 2, color: "lightgreen" },
-    { text: "Three", cols: 1, rows: 1, color: "lightpink" },
-    { text: "Four", cols: 2, rows: 1, color: "#DDBDF1" },
-  ];
-
-  projectList: Array<any>;
-  projectListContentful = {};
+  projectListContentfulObject = {};
+  public projectListContentful: Array<any> = [];
   listOfTaskObservable$: Observable<any>;
 
   constructor(
-    public dataCompanyService: DataCompanyService,
     public translationEnFrService: TranslationEnFrService,
     public contentfulDataService: ContentfulDataService
-  ) {}
-  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+  ) {
+    this.usedLanguage = this.translationEnFrService.language;
+    // get data from contentful,
+    this.listOfTasks(environment.contentfulConfig.contentTypeIds.tasks);
+
+  }
 
   designOption = "table_bootstrap";
   onChangeEvent(event) {
     this.designOption = event.target.value; // return option1
   }
-  transforDataTree() {
-    // tslint:disable-next-line:prefer-for-of
-    for (let i = 0; i < this.projectList.length; i++) {
-      this.TREE_DATA.push({
-        name: this.projectList[i].company,
-        logo: this.projectList[i].logo,
-        children: this.projectList[i].tasks,
-      });
-    }
-    return this.TREE_DATA;
-  }
-
+  
   listOfTasks(contenID) {
     this.contentfulDataService
       .getAllContaintfulTaskData(contenID)
       .subscribe((res) => {
-        this.projectListContentful = res.fields.tasksByRole;
-        console.log(this.projectListContentful);
-
+        console.log(res);
+        this.projectListContentfulObject = res.fields.tasksByRole;
+        console.log(this.projectListContentfulObject);
+        for (let i in this.projectListContentfulObject) {
+          //Pay attention to the 'in'
+          this.projectListContentful.push(this.projectListContentfulObject[i]);
+          for (let j in this.projectListContentful[i].fields.tasksListEn) {
+            this.projectListContentful[i].fields.tasksListEn[j] = {
+              name: this.projectListContentful[i].fields.tasksListEn[j],
+            };
+          }
+          for (let j in this.projectListContentful[i].fields.taskListFr) {
+            this.projectListContentful[i].fields.taskListFr[j] = {
+              name: this.projectListContentful[i].fields.taskListFr[j],
+            };
+          }
+        }
+        
+        for (let i = 0; i < this.projectListContentful.length; i++) {
+          TREE_DATA.push({
+            name: (this.usedLanguage == 'en' )? this.projectListContentful[i].fields.role : this.projectListContentful[i].fields.roleFr,
+            logo: this.projectListContentful[i].fields.logo.fields.file.url,
+            children: (this.usedLanguage == 'en' )? this.projectListContentful[i].fields.tasksListEn: this.projectListContentful[i].fields.taskListFr,
+          });
+        }
+        this.dataSource.data = TREE_DATA;
       });
   }
   ngOnInit() {
-    this.projectList = this.dataCompanyService.projectList;
-    this.dataCompanyService.getData();
-    this.transforDataTree();
-    this.dataSource.data = this.TREE_DATA;
-    // get data from contentful,
-    this.listOfTasks(environment.contentfulConfig.contentTypeIds.tasks);
+    TREE_DATA = [];
   }
 }
