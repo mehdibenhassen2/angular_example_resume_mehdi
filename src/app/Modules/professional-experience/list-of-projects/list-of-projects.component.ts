9bc6994add75f1bd5a5c61ac0934bb5d74c45fea
Mehdi BEN HASSEN <m.ben.hassen80@gmail.com>
2024-03-28 00:03:46 -0400

    MBH-13 update project

--- src/app/Modules/professional-experience/list-of-projects/list-of-projects.component.ts
+++ src/app/Modules/professional-experience/list-of-projects/list-of-projects.component.ts
@@ -1,27 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import {DataCompanyService } from '../../../services/data-company.service'
-import { TranslationEnFrService} from '../../../services/translation-en-fr.service';
-import {TranslateService} from '@ngx-translate/core';
+import { Component, OnInit } from "@angular/core";
+import { DataCompanyService } from "../../../services/data-company.service";
+import { TranslationEnFrService } from "../../../services/translation-en-fr.service";
+import { TranslateService } from "@ngx-translate/core";
 // import contentful service
 import { ContentfulDataService } from "./../../../services/contentful-data.service";
-import { environment } from 'src/environments/environment';
+import { environment } from "src/environments/environment";
 @Component({
-  selector: 'app-list-of-projects',
-  templateUrl: './list-of-projects.component.html',
-  styleUrls: ['./list-of-projects.component.scss']
+  selector: "app-list-of-projects",
+  templateUrl: "./list-of-projects.component.html",
+  styleUrls: ["./list-of-projects.component.scss"],
 })
 export class ListOfProjectsComponent implements OnInit {
-
   projectListContentfulObject = {};
   public projectListContentful: Array<any> = [];
   projectList: Array<any>;
-  
-  constructor(public dataCompanyService: DataCompanyService,
-              public translationEnFrService: TranslationEnFrService,
-              public contentfulDataService: ContentfulDataService) {
 
-       // get data from contentful,
-       this.listOfTasks(environment.contentfulConfig.contentTypeIds.tasks);
+  constructor(
+    public dataCompanyService: DataCompanyService,
+    public translationEnFrService: TranslationEnFrService,
+    public contentfulDataService: ContentfulDataService
+  ) {
+    // get data from contentful,
+    this.listOfTasks(environment.contentfulConfig.contentTypeIds.tasks);
   }
 
   listOfTasks(contenID) {
@@ -30,22 +30,25 @@ export class ListOfProjectsComponent implements OnInit {
       .subscribe((res) => {
         this.projectListContentfulObject = res.items[0].fields.tasksByRole;
         for (let i in this.projectListContentfulObject) {
-          
-           
           this.projectListContentful.push(this.projectListContentfulObject[i]);
         }
-        console.log('data:');
         console.log(this.projectListContentful);
-       
-
-  });
+      });
   }
-ngOnInit() {
-  this.projectList = this.dataCompanyService.projectList;
+  hideDisplayProject(i,j){
+ this.projectListContentful[i].fields.projects[j].visibility= ! this.projectListContentful[i].fields.projects[j].visibility;
+console.log(this.projectListContentful[i].fields.projects[j].visibility)
+  }
+  extendOrCollaspAll(extended){
+    for (let i in this.projectListContentful) {
+      for (let j in this.projectListContentful[i].fields.projects){
+        this.projectListContentful[i].fields.projects[j].visibility = extended;
+      }
+    }
+  }
+ 
 
-
-
-    
-}
-
+  ngOnInit() {
+    this.projectList = this.dataCompanyService.projectList;
+  }
 }
