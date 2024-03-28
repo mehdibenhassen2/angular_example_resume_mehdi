9442f0f5c6b6f00ab2ada1da4d85efa999b994bf
Mehdi BEN HASSEN <m.ben.hassen80@gmail.com>
2024-03-28 10:43:23 -0400

    MBH-16 clean the code

--- src/app/services/data-company.service.ts
+++ src/app/services/data-company.service.ts
@@ -1,13 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { UniversityOfCalgaryProject1Component } from 'src/app/Modules/professional-experience/list-of-projects/university-of-calgary-project1/university-of-calgary-project1.component';
-import { InsuredConnectProject1Component } from 'src/app/Modules/professional-experience/list-of-projects/insured-connect-project1/insured-connect-project1.component';
-import { ScotiabankProject1Component } from 'src/app/Modules/professional-experience/list-of-projects/scotiabank-project1/scotiabank-project1.component';
-import { ScotiabankProject2Component } from 'src/app/Modules/professional-experience/list-of-projects/scotiabank-project2/scotiabank-project2.component';
-import { GeomaticaProject1Component } from 'src/app/Modules/professional-experience/list-of-projects/geomatica-project1/geomatica-project1.component';
 // tslint:disable-next-line:max-line-length
-import { GeoenvironementProject1Component } from 'src/app/Modules/professional-experience/list-of-projects/geoenvironement-project1/geoenvironement-project1.component';
-import { ClickatellProject1Component } from 'src/app/Modules/professional-experience/list-of-projects/clickatell-project1/clickatell-project1.component';
 @Injectable({
   providedIn: 'root'
 })
@@ -219,145 +212,13 @@ export class DataCompanyService {
       }];
 
       
-    this.projectList = [
-      {
-        role: 'Senior Front End developer',
-        role_fr: 'Développeur Frontal Sénior',
-        company: 'Clickatell',
-        year: '2019- 2023',
-        logo: 'assets/logo/clickatell-logo-vector.png',
-       
-        projectCompany: [
-          {
-            projectIndex: '1',
-            type: 'Web App',
-            projectName: ' Build the next generation of Clickatell products using Angular, html, CSS, JavaScript',
-            projectName_fr: 'Construire une nouvell version des produits  clickatell en utilisant Angular, HTML, CSS, JavaScript',
-            projectdetails: {
-              data: ClickatellProject1Component,
-              visible: false,
-            },
-          },
-        ]
-      },
-      {
-        role: 'Senior Front End developer',
-        role_fr: 'Développeur Frontal Sénior',
-        company: 'Insured Connect',
-        year: '2018-2019',
-        logo: 'assets/logo/insured connect.png',
-                projectCompany: [
-          {
-            projectIndex: '1',
-            type: 'Web App',
-            projectName: 'Build four web applicaitons (for advisor, distributor, carrier, and policy owner) using Angular, html, CSS, JavaScript',
-            projectName_fr: 'Construire quatre applications Web (pour conseiller, distributeur, opérateur et propriétaire de police) en utilisant Angular, HTML, CSS, JavaScript',
-            projectdetails: {
-              data: InsuredConnectProject1Component,
-              visible: false,
-            },
-          },
-        ]
-      },
-      {
-        role: 'Front End developer/Manager',
-        role_fr: 'Développeur/Manager Frontal',
-        company: 'Scotiabank ',
-        year: '2016-2018',
-        logo: 'assets/logo/scotia.png',
-        
-        projectCompany: [
-          {
-            projectIndex: '1',
-            type: 'Mobile App',
-            projectName: 'Build Mobile application IOS for iPad using AngularJS, html, CSS, JavaScript.',
-            projectName_fr: 'Construire application mobile IOS pour iPad en utilisant AngularJS, HTML, CSS, JavaScript.',
-            projectdetails: {
-              data: ScotiabankProject1Component,
-              visible: false,
-            },
-          },
-          {
-            projectIndex: '2',
-            type: 'Web Map',
-            projectName: 'Configuration, installation, and development of Atlas Decision Support System.',
-            projectName_fr: 'Configuration, installation et développement du système d\'aide à la décision : Atlas.',
-            projectdetails: {
-              data: ScotiabankProject2Component,
-              visible: false,
-            },
-          }
-        ]
-      },
-      {
-        role: 'Technology Lead',
-        role_fr: 'Responsable technique',
-        company: 'Cansel Survey Equipment(Calgary) ',
-        year: '2015- 2016',
-        logo: 'assets/logo/logo-cansel.png',
-        projectCompany: [
-          {
-            projectIndex: '1',
-            type: 'Software app',
-            projectName: ' ',
-            projectdetails: {
-              data: '',
-              visible: false,
-            },
-          },
-        ]
-      },
-      {
-        role: 'Developer',
-        role_fr: 'Développeur',
-        company: 'University of Calgary ',
-        year: '2012-2014',
-        logo: 'assets/logo/University-Of-Calgary-Logo.png',
-        projectCompany: [
-          {
-            projectIndex: '1',
-            type: 'Software app',
-            projectName: ' Software development for Automatic extraction of Insulator feature from close range photogrammetry and laser scanner data.',
-            projectName_fr: 'Développement d\'un logiciel pour l\'extraction automatique des Insulators à partir de données de photogrammétrie et de scanner laser à courte portée.',
-            projectdetails: {
-              data: UniversityOfCalgaryProject1Component,
-              visible: false,
-            },
-          },
-        ]
-      },
-     
-    ];
+
   }
   public getData() {
     this.httpClient.get('../').subscribe(response => {
     }
     );
   }
-  public iconChange(i: number, j: number) {
-    this.projectList[i].projectCompany[j].projectdetails.visible = !this.projectList[i].projectCompany[j].projectdetails.visible;
-    // console.log(this.projectList[i].projectCompany[j].projectdetails.visible)
 
 
-  }
-  /**
-   * show all project details
-  * */
-  public showAll() {
-    for (const oneProject of this.projectList) {
-      for (const oneCompany of oneProject.projectCompany) {
-        oneCompany.projectdetails.visible = true;
-      }
-    }
-  }
-  /**
-   * hide all project details
-  * */
-  public hideAll() {
-    for (const oneProject of this.projectList) {
-      for (const oneCompany of oneProject.projectCompany) {
-        oneCompany.projectdetails.visible = false;
-      }
-    }
-  }
 }
